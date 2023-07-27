bd8145735c01c1e0b1ec2a9e9cd3ec73dbf58664
DongEmSi <ledongdong102@gmail.com>
2023-07-27 09:52:31 +0700

    chore: build microservice in docker

--- apps/driver/src/schemas/driver.schema.ts
+++ apps/driver/src/schemas/driver.schema.ts
@@ -4,13 +4,34 @@ import { AbstractDocument } from "y/common";
 @Schema({ versionKey: false}) 
 export class Driver extends AbstractDocument {
     @Prop()
-    name: string;
+    username: string;
 
+    @Prop()
+    fullname: string;
+  
+    @Prop({ unique: [true, 'Duplicate phone number entered'] })
+    phone: string;
+  
+    @Prop({ unique: [true, 'Duplicate email entered'] })
+    email: string;
+  
+    @Prop()
+    password: string;
+  
+    @Prop()
+    gender: string;
+  
+    @Prop()
+    dob: string;
+  
     @Prop()
     address: string;
+  
+    @Prop({ default: false })
+    blocked: boolean;
 
     @Prop()
-    number: number;
+    rating: number;
 }
 
 export const DriverSchema = SchemaFactory.createForClass(Driver);
